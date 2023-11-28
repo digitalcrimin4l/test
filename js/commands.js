3e7f887ef2d38ff0a3693a175096a9b3378fa65b
digitalcrimin4l <152237184+digitalcrimin4l@users.noreply.github.com>
2023-11-28 04:27:51 +0000

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,47 @@
+var github = "https://github.com/digitalcrimin4l";
+
+whois = [
+  "<br>",
+  "i love copying goodies and learning how it works",
+  "<br>"
+];
+
+
+social = [
+  "<br>",
+  'github         <a href="' + github + '" target="_blank">github/digitalcrimin4l' + "</a>",
+  "<br>"
+];
+
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is digitalcrimin4l',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Watch your step.</span>',
+  
+"                             (\,/)",
+"                             oo   '''//,        _",
+"                           ,/_;~,        \,    / '",
+"  digitalcrimin4l           '   \    (    \    ! " ,
+"           © 2023                ',|  \    |__.' " ,
+"                                '~  '~----''",
+  
+  '<span class="color2">lol i stole the whole idea.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
